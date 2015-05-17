267c659d2d5e6e47502ddc6975068c555589cf92
André Felipe Silveira <andrefelipesilveira@yahoo.com.br>
2015-05-17 15:42:51 -0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,23 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
 
+  has_many :microposts, dependent: :destroy
+  
   class << self
     # Returns the hash digest of the given string.
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
+      BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
 
     # Returns a random token.
-     def new_token
+    def new_token
       SecureRandom.urlsafe_base64
     end
 
@@ -56,7 +58,7 @@ class User < ActiveRecord::Base
   def create_reset_digest
     self.reset_token = User.new_token
     update_columns(reset_digest:  User.digest(reset_token),
-                   reset_sent_at: Time.zone.now)
+     reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
@@ -66,6 +68,10 @@ class User < ActiveRecord::Base
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
@@ -80,4 +86,4 @@ class User < ActiveRecord::Base
       self.activation_digest = User.digest(activation_token)
     end
 
-end
+  end
